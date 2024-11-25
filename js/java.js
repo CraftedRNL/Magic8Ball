a585c8b36e6c3af5dc27c1416768875a47072c74
Ryan <145123158+CraftedRNL@users.noreply.github.com>
2024-11-24 21:49:09 -0500

    Update java.js

--- js/java.js
+++ js/java.js
@@ -1,8 +1,22 @@
-var userName ="John Doe";
-// userName = window.prompt('Please Enter Your Name', '');
+var userName = "John Doe";
+userName = window.prompt('Please Enter Your Name', '');
+uName = document.getElementById('name');
+if (userName != null) {
+    uName.innerHTML = `Hello ${userName}`;
+} else {
+    uName.innerHTML = `Hello John Doe`;
+}
 
 
-document.getElementById('name').innerHTML =`hello ${userName}`;
+document.getElementById('circle').addEventListener("click", function () {
+    const overlay = document.getElementById('darkness');
+    overlay.style.opacity = 1;
+  overlay.style.transform = "scale(100)";
 
+    setTimeout(function () {
+        window.location.href = '8ball.html';
+    }, 900)
+   
+});
 
 
