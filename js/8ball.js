1f9fc5b1b4ba637c48df6ee8e86d6a3c0eaa7222
CraftedRNL <126Rliu@frhsd.com>
2024-11-26 12:54:29 -0500

    images

---
+++ js/8ball.js
@@ -0,0 +1,5 @@
+setTimeout(function () {
+        const overlay = document.getElementById('darkness2');
+        overlay.style.opacity = 1;
+      overlay.style.transform = "scale(0)";
+    }, 900);
